570fe0f9337264dab767733778621bd34da27bc7
PearsonJ <pearsonjoshua13131992@gmail.com>
2018-12-15 10:14:56 -0800

    getting one comment back and need to figure out how to get all of them back and then post them to the dom

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,41 +3,59 @@ module.exports = function(app) {
   var cheerio = require("cheerio");
   var axios = require("axios");
 
-app.post("/", function (req, res) {
+app.get("/", function (req, res) {
   
 
-axios.get("https://politics.theonion.com/")
-  .then(function(response) {
+  axios.get("https://politics.theonion.com/")
+    .then(function(response) {
+      console.log("cheerio");
+  
+    var $ = cheerio.load(response.data);
 
- 
-  var $ = cheerio.load(response.data);
+    var increment = 0;
+    $("article.postlist__item").each(function(i, element) {
+
+      var title = $(element).children("header").children("h1.headline").text();
+      var link = $(element).children("header").find("a").attr("href");
+      var summary = $(element).children("div.item__content").find("div.excerpt").text();
 
 
-  $("article.postlist__item").each(function(i, element) {
-
-    var title = $(element).children("header").children("h1.headline").text();
-    var link = $(element).children("header").find("a").attr("href");
-    var summary = $(element).children("div.item__content").find("div.excerpt").text()
-
-  db.Article.create({title: title, link: link, summary: summary}, function (err, submitted) {
-    if (err) {
-      console.log(err)
-    } else {
-      console.log(submitted);
-    }
-  });
+      db.Article.create({title: title, link: link, summary: summary}, function (err, submitted) {
+        if (err) {
+          // console.log(err)
+        } else {
+          // console.log(submitted);
+        }
+        console.log(increment, $("article.postlist__item").length);
+        if(increment >= $("article.postlist__item").length - 1){
+          renderIndex(req, res);
+        } else {
+          increment++;
+        }
+      });
+    });
   });
 });
-});
 
-app.get("/read", function (req, res) {
-  db.Article.find({},function(err, found) {
-    if(err){
-      console.log(err)
-    } else{
+function renderIndex (req, res) {
+
+  db.Article.find({}).populate("comment").then(function(found) {
+      console.log(found);
       res.render("index", {data: found})
-    }
-  });
-});
+  }).catch(function(err){
+    console.log(err);
+  }
+  )
+};
+
+app.post("/api/comment", function(req, res) {
+  db.comment.create(req.body).then(function(dbcomment){
+    var postData = req.body;
+    console.log(postData);
+    return db.Article.findOneAndUpdate({_id: postData.postId}, {$push: {user: dbcomment.user, comment: dbcomment.comment }}, {new: true}, function(dbResult){
+      renderIndex(req, res);
+    });
+  })
+})
 
 };
